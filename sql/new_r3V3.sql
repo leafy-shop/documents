8e19c51b289606c3cb15c775e4dc759d8e431ade
sahathat <61311677+sahathat@users.noreply.github.com>
2024-02-16 22:55:34 +0700

    Update new_r3V3.sql

--- sql/new_r3V3.sql
+++ sql/new_r3V3.sql
@@ -22,24 +22,27 @@ USE `leafy` ;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `leafy`.`accounts` (
   `userId` INT NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(50) NOT NULL,
-  `firstname` VARCHAR(50) NULL DEFAULT NULL,
-  `lastname` VARCHAR(50) NULL DEFAULT NULL,
+  `username` VARCHAR(20) NOT NULL,
+  `firstname` VARCHAR(50) NOT NULL,
+  `lastname` VARCHAR(50) NOT NULL,
   `description` VARCHAR(500) NULL DEFAULT NULL,
   `email` VARCHAR(100) NOT NULL,
   `password` VARCHAR(100) NOT NULL,
   `role` VARCHAR(11) NOT NULL DEFAULT 'user',
-  `status` TINYINT(1) NOT NULL DEFAULT '1',
-  `verifyAccount` TINYINT(1) NOT NULL DEFAULT '0',
+  `status` TINYINT NOT NULL DEFAULT '1',
+  `verifyAccount` TINYINT NOT NULL DEFAULT '0',
   `phone` CHAR(11) NULL DEFAULT NULL,
-  `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-  `updatedAt` DATETIME(3) NOT NULL,
+  `createdAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`userId`),
-  UNIQUE INDEX `Users_email_key` (`email` ASC) VISIBLE)
+  UNIQUE INDEX `Users_email_key` (`email` ASC) VISIBLE,
+  UNIQUE INDEX `Fullname_UNIQUE` (`firstname` ASC, `lastname` ASC) INVISIBLE,
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE,
+  UNIQUE INDEX `phone_UNIQUE` (`phone` ASC) VISIBLE)
 ENGINE = InnoDB
-AUTO_INCREMENT = 3004
+AUTO_INCREMENT = 3006
 DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_ci;
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
@@ -47,24 +50,23 @@ COLLATE = utf8mb4_unicode_ci;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `leafy`.`addresses` (
   `addressId` CHAR(32) NOT NULL,
-  `userEmail` VARCHAR(100) NOT NULL,
   `addressname` VARCHAR(100) NOT NULL,
-  `phone` CHAR(11) NULL DEFAULT NULL,
   `address` VARCHAR(50) NOT NULL,
   `province` VARCHAR(20) NOT NULL,
   `distrinct` VARCHAR(20) NOT NULL,
   `subDistrinct` VARCHAR(20) NULL DEFAULT NULL,
   `postalCode` CHAR(5) NOT NULL,
+  `userEmail` VARCHAR(100) NOT NULL,
+  `phone` CHAR(11) NULL DEFAULT NULL,
   PRIMARY KEY (`addressId`),
-  INDEX `Addresses_userEmail_fkey` (`userEmail` ASC) VISIBLE,
-  CONSTRAINT `Addresses_userEmail_fkey`
+  INDEX `fk_addresses_accounts1_idx` (`userEmail` ASC) VISIBLE,
+  CONSTRAINT `fk_addresses_accounts1`
     FOREIGN KEY (`userEmail`)
     REFERENCES `leafy`.`accounts` (`email`)
-    ON DELETE RESTRICT
-    ON UPDATE CASCADE)
+    ON DELETE CASCADE)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_ci;
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
@@ -73,28 +75,27 @@ COLLATE = utf8mb4_unicode_ci;
 CREATE TABLE IF NOT EXISTS `leafy`.`items` (
   `itemId` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(100) NOT NULL,
-  `description` VARCHAR(5000) NOT NULL,
-  `itemOwner` VARCHAR(191) NOT NULL,
+  `description` VARCHAR(5000) NULL DEFAULT NULL,
+  `itemOwner` VARCHAR(100) NOT NULL,
   `type` VARCHAR(20) NOT NULL,
   `tag` VARCHAR(500) NOT NULL,
   `totalRating` DECIMAL(2,1) NOT NULL DEFAULT '0.0',
   `sold` INT NOT NULL DEFAULT '0',
   `minPrice` DECIMAL(32,2) NOT NULL DEFAULT '0.00',
   `maxPrice` DECIMAL(32,2) NULL DEFAULT NULL,
-  `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-  `updatedAt` DATETIME(3) NOT NULL,
+  `createdAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`itemId`),
-  INDEX `fk_items_users1_idx` (`itemId` ASC) VISIBLE,
-  INDEX `Items_itemOwner_fkey` (`itemOwner` ASC) VISIBLE,
-  CONSTRAINT `Items_itemOwner_fkey`
+  INDEX `fk_items_users1_idx` (`itemOwner` ASC) VISIBLE,
+  CONSTRAINT `fk_items_users1`
     FOREIGN KEY (`itemOwner`)
     REFERENCES `leafy`.`accounts` (`email`)
     ON DELETE RESTRICT
-    ON UPDATE CASCADE)
+    ON UPDATE RESTRICT)
 ENGINE = InnoDB
 AUTO_INCREMENT = 300054
 DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_ci;
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
@@ -103,14 +104,14 @@ COLLATE = utf8mb4_unicode_ci;
 CREATE TABLE IF NOT EXISTS `leafy`.`carts` (
   `cartId` INT NOT NULL AUTO_INCREMENT,
   `userEmail` VARCHAR(100) NOT NULL,
-  `itemId` INT NOT NULL,
+  `productId` INT NOT NULL,
   `qty` INT NOT NULL DEFAULT '0',
   `isPaid` TINYINT(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`cartId`),
-  INDEX `fk_carts_items_idx` (`itemId` ASC) VISIBLE,
+  INDEX `fk_carts_products_idx` (`productId` ASC) VISIBLE,
   INDEX `fk_carts_users_idx` (`userEmail` ASC) VISIBLE,
-  CONSTRAINT `Carts_itemId_fkey`
-    FOREIGN KEY (`itemId`)
+  CONSTRAINT `Carts_productId_fkey`
+    FOREIGN KEY (`productId`)
     REFERENCES `leafy`.`items` (`itemId`)
     ON DELETE RESTRICT
     ON UPDATE CASCADE,
@@ -120,8 +121,9 @@ CREATE TABLE IF NOT EXISTS `leafy`.`carts` (
     ON DELETE RESTRICT
     ON UPDATE CASCADE)
 ENGINE = InnoDB
+AUTO_INCREMENT = 11
 DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_ci;
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
@@ -131,94 +133,97 @@ CREATE TABLE IF NOT EXISTS `leafy`.`favprd` (
   `userEmail` VARCHAR(100) NOT NULL,
   `itemId` INT NOT NULL,
   PRIMARY KEY (`itemId`, `userEmail`),
-  INDEX `fk_FavPRD_items1_idx` (`itemId` ASC) VISIBLE,
   INDEX `fk_FavPRD_users1_idx` (`userEmail` ASC) VISIBLE,
-  CONSTRAINT `FavPrd_itemId_fkey`
+  INDEX `fk_FavPRD_products1_idx` (`itemId` ASC) VISIBLE,
+  CONSTRAINT `FavPrd_productId_fkey`
     FOREIGN KEY (`itemId`)
     REFERENCES `leafy`.`items` (`itemId`)
-    ON DELETE RESTRICT
+    ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `FavPrd_userEmail_fkey`
     FOREIGN KEY (`userEmail`)
     REFERENCES `leafy`.`accounts` (`email`)
-    ON DELETE RESTRICT
+    ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_ci;
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
 -- Table `leafy`.`item_details`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `leafy`.`item_details` (
+  `style` VARCHAR(50) NOT NULL DEFAULT 'No',
   `itemId` INT NOT NULL,
-  `style` VARCHAR(50) NOT NULL,
   `stock` INT NOT NULL DEFAULT '0',
-  `size` VARCHAR(4) NOT NULL DEFAULT 'No',
+  `size` CHAR(4) NOT NULL DEFAULT 'No',
   `price` DECIMAL(32,2) NOT NULL DEFAULT '0.00',
-  PRIMARY KEY (`itemId`, `style`, `size`),
-  INDEX `fk_ItemDetail_items_idx` (`itemId` ASC, `style` ASC, `size` ASC) VISIBLE)
+  PRIMARY KEY (`style`, `itemId`, `size`),
+  INDEX `fk_table1_items1_idx` (`itemId` ASC) VISIBLE,
+  CONSTRAINT `fk_table1_items1`
+    FOREIGN KEY (`itemId`)
+    REFERENCES `leafy`.`items` (`itemId`)
+    ON DELETE CASCADE)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_ci;
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
 -- Table `leafy`.`item_events`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `leafy`.`item_events` (
+  `timestamp` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
   `userId` INT NOT NULL,
   `itemId` INT NOT NULL,
   `itemEvent` CHAR(4) NOT NULL,
-  `timestamp` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-  INDEX `fk_EventItem_items1_idx` (`itemId` ASC) VISIBLE,
-  INDEX `fk_EventItem_users1_idx` (`userId` ASC) VISIBLE,
   PRIMARY KEY (`timestamp`),
-  CONSTRAINT `EventItem_itemId_fkey`
-    FOREIGN KEY (`itemId`)
-    REFERENCES `leafy`.`items` (`itemId`)
-    ON DELETE RESTRICT
-    ON UPDATE CASCADE,
-  CONSTRAINT `EventItem_userEmail_fkey`
+  INDEX `fk_item_events_accounts1_idx` (`userId` ASC) VISIBLE,
+  INDEX `fk_item_events_items1_idx` (`itemId` ASC) VISIBLE,
+  CONSTRAINT `fk_item_events_accounts1`
     FOREIGN KEY (`userId`)
     REFERENCES `leafy`.`accounts` (`userId`)
-    ON DELETE RESTRICT
-    ON UPDATE CASCADE)
+    ON DELETE CASCADE
+    ON UPDATE RESTRICT,
+  CONSTRAINT `fk_item_events_items1`
+    FOREIGN KEY (`itemId`)
+    REFERENCES `leafy`.`items` (`itemId`)
+    ON DELETE CASCADE
+    ON UPDATE RESTRICT)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_ci;
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
 -- Table `leafy`.`item_reviews`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `leafy`.`item_reviews` (
-  `itemReviewId` CHAR(32) NOT NULL,
+  `itemReviewId` VARCHAR(32) NOT NULL,
   `itemId` INT NOT NULL,
   `userEmail` VARCHAR(100) NOT NULL,
   `comment` VARCHAR(500) NOT NULL,
-  `rating` INT NOT NULL DEFAULT '0',
+  `rating` INT NOT NULL,
   `like` INT NOT NULL DEFAULT '0',
   `style` VARCHAR(50) NOT NULL,
+  `createdAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `size` VARCHAR(4) NOT NULL DEFAULT 'No',
-  `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
   PRIMARY KEY (`itemReviewId`),
-  INDEX `ItemReview_itemId_fkey` (`itemId` ASC) VISIBLE,
-  INDEX `Itemreview_userEmail_fkey` (`userEmail` ASC) VISIBLE,
-  CONSTRAINT `ItemReview_itemId_fkey`
-    FOREIGN KEY (`itemId`)
-    REFERENCES `leafy`.`items` (`itemId`)
-    ON DELETE RESTRICT
-    ON UPDATE CASCADE,
-  CONSTRAINT `Itemreview_userEmail_fkey`
+  INDEX `fk_item_preview_items1_idx` (`itemId` ASC) VISIBLE,
+  INDEX `fk_item_preview_accounts1_idx` (`userEmail` ASC) VISIBLE,
+  CONSTRAINT `fk_item_preview_accounts1`
     FOREIGN KEY (`userEmail`)
     REFERENCES `leafy`.`accounts` (`email`)
-    ON DELETE RESTRICT
-    ON UPDATE CASCADE)
+    ON UPDATE RESTRICT,
+  CONSTRAINT `fk_item_preview_items1`
+    FOREIGN KEY (`itemId`)
+    REFERENCES `leafy`.`items` (`itemId`)
+    ON DELETE CASCADE
+    ON UPDATE RESTRICT)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_ci;
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
@@ -226,23 +231,20 @@ COLLATE = utf8mb4_unicode_ci;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `leafy`.`item_review_likes` (
   `userEmail` VARCHAR(100) NOT NULL,
-  `itemReviewId` CHAR(32) NOT NULL,
-  PRIMARY KEY (`itemReviewId`, `userEmail`),
-  INDEX `fk_UserReviewLike_items_review1_idx` (`itemReviewId` ASC) VISIBLE,
-  INDEX `fk_UserReviewLike_users1_idx` (`userEmail` ASC) VISIBLE,
-  CONSTRAINT `UserreviewLike_itemreviewId_fkey`
-    FOREIGN KEY (`itemReviewId`)
-    REFERENCES `leafy`.`item_reviews` (`itemReviewId`)
-    ON DELETE RESTRICT
-    ON UPDATE CASCADE,
-  CONSTRAINT `UserreviewLike_userEmail_fkey`
+  `itemReviewId` VARCHAR(32) NOT NULL,
+  PRIMARY KEY (`userEmail`, `itemReviewId`),
+  INDEX `fk_item_preview_like_item_preview1_idx` (`itemReviewId` ASC) VISIBLE,
+  CONSTRAINT `fk_item_preview_like_accounts1`
     FOREIGN KEY (`userEmail`)
     REFERENCES `leafy`.`accounts` (`email`)
-    ON DELETE RESTRICT
-    ON UPDATE CASCADE)
+    ON DELETE CASCADE,
+  CONSTRAINT `fk_item_preview_like_item_preview1`
+    FOREIGN KEY (`itemReviewId`)
+    REFERENCES `leafy`.`item_reviews` (`itemReviewId`)
+    ON DELETE CASCADE)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_ci;
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
@@ -250,20 +252,18 @@ COLLATE = utf8mb4_unicode_ci;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `leafy`.`payments` (
   `paymentId` CHAR(32) NOT NULL,
-  `userEmail` VARCHAR(100) NOT NULL,
   `bankname` VARCHAR(100) NOT NULL,
   `bankCode` VARCHAR(10) NOT NULL,
   `bankAccount` VARCHAR(16) NOT NULL,
+  `userEmail` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`paymentId`),
-  INDEX `Payments_userEmail_fkey` (`userEmail` ASC) VISIBLE,
-  CONSTRAINT `Payments_userEmail_fkey`
+  INDEX `fk_payments_accounts1_idx` (`userEmail` ASC) VISIBLE,
+  CONSTRAINT `fk_payments_accounts1`
     FOREIGN KEY (`userEmail`)
-    REFERENCES `leafy`.`accounts` (`email`)
-    ON DELETE RESTRICT
-    ON UPDATE CASCADE)
+    REFERENCES `leafy`.`accounts` (`email`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_ci;
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
