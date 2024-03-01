575f75aa8a71a55751bc89ce1f95f19026d13d3a
sahathat <61311677+sahathat@users.noreply.github.com>
2024-03-01 22:13:23 +0700

    Update temporally_cart.sql
    - add temporally cart mode
    - when update cart if they must not update their cart by in 7 days they must be delete on server

--- sql/temporally_cart.sql
+++ sql/temporally_cart.sql
@@ -6,7 +6,16 @@ CREATE EVENT delete_expired_data
 ON SCHEDULE EVERY 1 SECOND -- Adjust the interval as needed
 DO
     DELETE FROM session_cart
-    WHERE updatedAt < NOW() - INTERVAL 7 DAY; -- Adjust the expiration time as needed
+    WHERE updatedAt < NOW() - INTERVAL 10 SECOND -- Adjust the expiration time as needed
+;
+
+CREATE EVENT delete_expired_data
+ON SCHEDULE EVERY 1 SECOND -- Adjust the interval as needed
+DO
+    DELETE FROM session_cart
+    WHERE updatedAt < NOW() - INTERVAL 7 DAY -- Adjust the expiration time as needed
 ;
 
 SHOW EVENTS from leafy;
+
+DROP EVENT delete_expired_data;
